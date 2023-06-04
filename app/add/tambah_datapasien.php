cae32da7f7283a4c762aeaf5c670a5514aed176e
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2023-06-04 20:13:03 +0700

    Update tambah_datapasien.php

--- app/add/tambah_datapasien.php
+++ app/add/tambah_datapasien.php
@@ -3,12 +3,5 @@ include('../../conf/config.php');
 // var_dump($_POST);
 // exit;
 $query = mysqli_query($koneksi, "INSERT INTO datapasien (nik, nama, jeniskelamin, goldarah, alamat, tanggallahir, umur, nomorhp, tglperiksa, puskesmas, riwayat) 
-VALUES('$_POST[nik]', '$_POST[nama]', '$_POST[jeniskelamin]', '$_POST[goldarah]',
-'$_POST[alamat]',
-'$_POST[tanggallahir]',
-'$_POST[umur]',
-'$_POST[nomorhp]',
-'$_POST[tglperiksa]',
-'$_POST[puskesmas]',
-'$_POST[riwayat]')");
+VALUES('$_POST[nik]', '$_POST[nama]', '$_POST[jeniskelamin]', '$_POST[goldarah]','$_POST[alamat]','$_POST[tanggallahir]','$_POST[umur]','$_POST[nomorhp]','$_POST[tglperiksa]','$_POST[puskesmas]','$_POST[riwayat]')");
 header('Location: ../index.php?page=datapasien');
