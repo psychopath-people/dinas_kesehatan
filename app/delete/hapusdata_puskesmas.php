e01ed0385f94a156354cec518589427f550cb770
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2023-06-05 21:17:33 +0700

    Update hapusdata_puskesmas.php

--- app/delete/hapusdata_puskesmas.php
+++ app/delete/hapusdata_puskesmas.php
@@ -1,7 +1,7 @@
 <?php
 include('../../conf/config.php');
 $id = $_GET['id'];
-
-$query = mysqli_query($koneksi,"DELETE FROM datapuskesmas WHERE id='$id'");
+var_dump($id);
+exit;
+$query = mysqli_query($koneksi, "DELETE FROM datapuskesmas WHERE id='$id'");
 header('Location: ../index.php?page=datapuskesmas');
-?>
