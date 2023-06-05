aac72b226ee5168cd3c376ad86e6fed463879c58
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2023-06-05 15:23:33 +0700

    Update tambah_datapasien.php

--- app/add/tambah_datapasien.php
+++ app/add/tambah_datapasien.php
@@ -1,6 +1,6 @@
 <?php
 include('../../conf/config.php');
-// var_dump($_POST);
+// var_dump($_POST['nik']);
 // exit;
 $query = mysqli_query($koneksi, "INSERT INTO datapasien (nik, nama, jeniskelamin, goldarah, alamat, tanggallahir, umur, nomorhp, tglperiksa, puskesmas, riwayat) 
 VALUES('$_POST[nik]', '$_POST[nama]', '$_POST[jeniskelamin]', '$_POST[goldarah]','$_POST[alamat]','$_POST[tanggallahir]','$_POST[umur]','$_POST[nomorhp]','$_POST[tglperiksa]','$_POST[puskesmas]','$_POST[riwayat]')");
