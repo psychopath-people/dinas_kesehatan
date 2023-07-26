8e5d42cfcc968ef8f149f9eb2d226a8be0140a64
psychopath-people <117507902+psychopath-people@users.noreply.github.com>
2023-07-26 11:21:10 +0700

    Update dashboard.php

--- app/dashboard.php
+++ app/dashboard.php
@@ -23,16 +23,30 @@
     </div><!-- /.container-fluid -->
     <?php
     $query = "SELECT DATE_FORMAT(tglperiksa, '%Y-%m') AS tanggal, COUNT(*) AS jumlah FROM datapasien GROUP BY DATE_FORMAT(tglperiksa, '%Y-%m')";
+    $query1 = "SELECT puskesmas, COUNT(*) AS jumlah FROM pasien GROUP BY puskesmas";
     $result = $koneksi->query($query);
+    $result1 = $koneksi->query($query1);
+
+
+    
 
     // Inisialisasi array untuk menyimpan data bulan dan jumlah datadatapasien
     $labels = [];
     $data = [];
 
+    $labels1 = [];
+    $data1 = [];
+
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $labels[] = $row['tanggal'];
             $data[] = $row['jumlah'];
+        }
+    }
+    if ($result1->num_rows > 0) {
+        while ($row = $result1->fetch_assoc()) {
+            $labels1[] = $row['puskesmas'];
+            $data1[] = $row['jumlah'];
         }
     }
 
@@ -44,6 +58,32 @@
         // Data dari PHP disimpan dalam variabel JavaScript
         var labels = <?php echo json_encode($labels); ?>;
         var data = <?php echo json_encode($data); ?>;
+
+        // Membuat grafik menggunakan Chart.js
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Jumlah datapasien',
+                    data: data,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+         // Data dari PHP disimpan dalam variabel JavaScript
+        var labels1 = <?php echo json_encode($labels1); ?>;
+        var data1 = <?php echo json_encode($data1); ?>;
 
         // Membuat grafik menggunakan Chart.js
         var ctx = document.getElementById('myChart').getContext('2d');
